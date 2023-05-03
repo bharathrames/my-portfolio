5d3b5176fcb281def1d3fd8f34093e2dd08ccf97
DhanushChinnaiyan <dhanushmd4021@gmail.com>
2023-05-03 18:31:22 +0530

    contact form structure changed

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,4 +1,4 @@
-import React,{ useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './contact.css'
 import emailjs from '@emailjs/browser';
 
@@ -7,53 +7,60 @@ import emailjs from '@emailjs/browser';
 
 
 const Contact = () => {
-  const [username,setUsername] = useState("");
-  const [useremail,setUseremail] = useState("");
-  const [usermessage,setUsermessage] = useState("");
+  const [username, setUsername] = useState("");
+  const [useremail, setUseremail] = useState("");
+  const [usermessage, setUsermessage] = useState("");
+  const [message, setMessage] = useState()
 
 
   const form = useRef();
 
   const sendEmail = (e) => {
+    setMessage()
     e.preventDefault();
 
     emailjs.sendForm('service_6nnf7nh', 'template_raprwgg', form.current, 'f1LguMy0z0BDG-yOs')
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
 
-
-      setUsername("");
-      setUseremail("");
-      setUsermessage("")
+    setMessage("Message sent successfully")
+    setUsermessage("")
   };
 
 
   return (
     <div className="contactdiv" id='Contact'>
-        <div className="contactheading">
-            Contact Us :
-        </div>
+      <div className="contactheading">
+        Contact Us :
+      </div>
+     
 
-       
-        <div className="formdiv">
-           {/* blur div */}
-       <div className='blur contactblur' style={{ background: "rgb(238 210 255)" }}></div>
-            <div className="contactinputdiv">
-            <form ref={form} onSubmit={sendEmail}>
-               
-               <input type="text" name="user_name" placeholder='Name :' value={username}  onChange={(event)=>setUsername(event.target.value)} required/>
-               
-               <input type="email" name="user_email" placeholder='Email :' value={useremail} onChange={(event)=>setUseremail(event.target.value)} required/>
-               
-               <textarea name="message" cols="50" rows="7" placeholder='Message :' value={usermessage} onChange={(event)=>setUsermessage(event.target.value)} required></textarea>
-               
-               <input className='button' type="submit"  value="Send"/>
-               </form>
-            </div>
+      <div className="formdiv">
+     
+        {/* blur div */}
+        <div className='blur contactblur' style={{ background: "rgb(238 210 255)" }}></div>
+  
+        <div className="contactinputdiv">
+           
+          <form ref={form} onSubmit={sendEmail}>
+
+            <input type="text" name="user_name" placeholder='Name :' value={username} onChange={(event) => setUsername(event.target.value)} required />
+
+            <input type="email" name="user_email" placeholder='Email :' value={useremail} onChange={(event) => setUseremail(event.target.value)} required />
+
+            <textarea name="message" cols="50" rows="7" placeholder='Message :' value={usermessage} onChange={(event) => setUsermessage(event.target.value)} required></textarea>
+ 
+            {message?<input className='button' type="submit" value="Send Again" />
+            :
+            <input className='button' type="submit" value="Send" />
+            }
+            {message && <div className="message">{message}</div>}
+          </form>
         </div>
+      </div>
     </div>
   )
 }
