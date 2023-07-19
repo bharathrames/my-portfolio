8a5c8776aa658874e3968cc729881a809021923c
bharathrames <bharath91505@gmail.com>
2023-07-19 19:13:29 +0530

    projectadded

--- src/components/project/projectdata.js
+++ src/components/project/projectdata.js
@@ -1,7 +1,8 @@
 import foodCompany from '../../images/foodcompany.png'
 import wisdomUrl from '../../images/wisdomurl.png'
-// import moneyManager from '../../images/moneymanager.png'
-// import studySpot from '../../images/studyspotcapital.png'
+import chatapp from '../../images/chatapp.jpg'
+import rent from '../../images/rent.png'
+
 
 const projectdata =[
     {
@@ -12,26 +13,26 @@ const projectdata =[
         deployedLink:"https://foodslist-app.netlify.app"
     },
     {
+        projectName : "chat-app",
+        projectImage : chatapp,
+        frontendgithubLink : "https://github.com/bharathrames/chatfrontend",
+        backendgithubLink:"https://github.com/bharathrames/chat-backend",
+        deployedLink:"https://es-chit-chat-app.netlify.app/"
+    },
+    {
+        projectName : "eqiupment-rental-app",
+        projectImage : rent,
+        frontendgithubLink : "https://github.com/bharathrames/2webcodefrontend",
+        backendgithubLink:"https://github.com/bharathrames/2webcodebackend",
+        deployedLink:"https://eqiumentrentalapp.netlify.app/"
+    },
+    {
         projectName : "Teachers & students list",
         projectImage : wisdomUrl,
         frontendgithubLink : "https://github.com/bharathrames/day31-student-teacher-crud",
         // backendgithubLink:"",
         deployedLink:"https://students-teacher-list-app.netlify.app"
-    },
-    // {
-    //     projectName : "",
-    //     projectImage : ,
-    //     frontendgithubLink : "",
-    //     backendgithubLink:"",
-    //     deployedLink:""
-    // },
-    // {
-    //     projectName : "",
-    //     projectImage : ,
-    //     frontendgithubLink : "",
-    //     backendgithubLink:"",
-    //     deployedLink:""
-    // }
+    }
     
 ]
 
